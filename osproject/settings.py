a993aceea0b49c7025cb33c184f97d6291f3b62d
bdarnold58 <bdarnold58@gmail.com>
2022-07-14 15:48:07 -0700

    disabled debug

--- osproject/settings.py
+++ osproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
